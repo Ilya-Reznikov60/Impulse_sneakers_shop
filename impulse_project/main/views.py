80a71d51aafb9c25162b9da6a00f8d1a10644e15
Ilya-Reznikov <reznikov.iluxa@yandex.ru>
2024-02-22 18:09:45 +0300

    Add index.html

--- impulse_project/main/views.py
+++ impulse_project/main/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    '''
+    Index function
+    '''
+    return render(request, 'index.html',)
+
+
+def about(request):
+    '''
+    about function
+    '''
+    return HttpResponse('About Page')
