105b8b050102091c8521ba050894d9ab88cadb65
Ilya-Reznikov <reznikov.iluxa@yandex.ru>
2024-03-03 17:05:29 +0300

    Add fixtures

--- impulse_project/main/views.py
+++ impulse_project/main/views.py
@@ -2,12 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
+from goods.models import Category
+
+
 def index(request):
     '''
     Index function
     '''
+    categories = Category.objects.all()
     context = {
-        'title': 'Impulse - Главная'
+        'title': 'Impulse - Главная',
+        'categories': categories,
     }
 
     return render(request, 'main/index.html', context)
