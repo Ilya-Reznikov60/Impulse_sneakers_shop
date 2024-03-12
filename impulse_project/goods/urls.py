d9cf63b76d5b31385e23a26b1dfa3b8430c9de9b
Ilya-Reznikov <reznikov.iluxa@yandex.ru>
2024-03-12 14:40:43 +0300

    Add the category filter the header.html

--- impulse_project/goods/urls.py
+++ impulse_project/goods/urls.py
@@ -5,6 +5,7 @@ from goods import views
 app_name = 'goods'
 
 urlpatterns = [
+    path('<slug:category_slug>/', views.catalog_by_category, name='catalog_by_category'),
     path('', views.catalog, name='index'),
     path('product/<slug:product_slug>/', views.product, name='product'),
 ]
--- impulse_project/goods/views.py
+++ impulse_project/goods/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from goods.models import Product, Specification
+from goods.models import Product, Specification, Category
 
 
 def catalog(request):
@@ -36,3 +36,14 @@ def product(request, product_slug):
     }
 
     return render(request, 'goods/product.html', context=context)
+
+
+def catalog_by_category(request, category_slug):
+    if category_slug == 'all':
+        products = Product.objects.all()
+        category = None
+    else:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=category)
+
+    return render(request, 'goods/catalog.html', {'goods': products, 'category': category})
