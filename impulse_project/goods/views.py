70a789bd3f9d72437e6ffb9f36d3dd16f67e9e37
Ilya-Reznikov <reznikov.iluxa@yandex.ru>
2024-03-15 12:04:44 +0300

    Add the pagination

--- impulse_project/goods/views.py
+++ impulse_project/goods/views.py
@@ -1,6 +1,16 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
 from goods.models import Product, Specification, Category
+
+
+NUMBER_OF_GOODS = 3
+
+
+def get_page(request, goods_list):
+    paginator = Paginator(goods_list, NUMBER_OF_GOODS)
+    page_number = request.GET.get('page')
+    return paginator.get_page(page_number)
 
 
 def catalog(request):
@@ -9,10 +19,12 @@ def catalog(request):
     '''
     goods = Product.objects.all()
 
+    page_obj = get_page(request, goods)
+
     context = {
         'title': 'Impulse - Каталог',
         'is_catalog_page': True,
-        'goods': goods,
+        'goods': page_obj,
     }
 
     return render(request, 'goods/catalog.html', context)
@@ -46,7 +58,9 @@ def catalog_by_category(request, category_slug):
         category = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=category)
 
+    page_obj = get_page(request, products)
+
     return render(
         request, 'goods/catalog.html',
-        {'goods': products, 'category': category}
+        {'goods': page_obj, 'category': category}
     )
